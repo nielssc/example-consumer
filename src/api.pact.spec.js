4a76dee30b8dbb11a79879dbad82d1cb021b1a00
Niels Schütte <niels.schuette@bosch-softtec.com>
2022-02-18 19:23:18 +0100

    change name

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -5,7 +5,7 @@ import { Product } from './product';
 const { eachLike, like, regex } = Matchers;
 
 const mockProvider = new Pact({
-  consumer: 'pactflow-example-consumer',
+  consumer: 'pactflow-example-consumer-niels',
   provider: process.env.PACT_PROVIDER ? process.env.PACT_PROVIDER : 'pactflow-example-provider',
 });
 
