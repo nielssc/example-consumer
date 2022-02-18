fc2af2d7aec48b8cf96378e86d839bc11c679568
Niels Schütte <niels.schuette@bosch-softtec.com>
2022-02-18 19:15:02 +0100

    fix

--- src/api.pact.spec.js
+++ src/api.pact.spec.js
@@ -17,7 +17,7 @@ describe('API Pact test', () => {
   describe('retrieving a product', () => {
     test('ID 10 exists', async () => {
       // Arrange
-      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', colour = 'red'}
+      const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', colour: 'red'}
 
       // Uncomment to see this fail
       // const expectedProduct = { id: '10', type: 'CREDIT_CARD', name: '28 Degrees', price: 30.0, newField: 22}
